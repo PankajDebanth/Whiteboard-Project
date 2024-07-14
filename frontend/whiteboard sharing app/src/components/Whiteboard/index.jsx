886fa5c0ad7e1da69be5ec87b081e381975c4344
Pankaj Debnath <pnkzdn580@gmail.com>
2024-07-14 22:21:40 +0530

    adding logic for line making

--- frontend/whiteboard sharing app/src/components/Whiteboard/index.jsx
+++ frontend/whiteboard sharing app/src/components/Whiteboard/index.jsx
@@ -1,32 +1,61 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useLayoutEffect } from "react";
 import rough from "roughjs";
-const Whiteboard = ({ canvasRef, ctxRef, Elements, setElements }) => {
+
+const roughGenerator = rough.generator();
+const Whiteboard = ({ canvasRef, ctxRef, elements, setElements }) => {
   const [isDrawing, setIsDrawing] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-
     ctxRef.current = ctx;
   }, []);
 
+  useLayoutEffect(()=>{
+    const roughCanvas = rough.canvas(canvasRef.current);
+    elements.forEach((element)=>{
+      roughCanvas.linearPath(element.path);
+    })
+  }, [elements])
+
   const handleMouseDown = (e) => {
     const { offsetX, offsetY } = e.nativeEvent;
-    console.log("offsetX", offsetX);
-    console.log("offsetY", offsetY);
+    setElements((prevELements)=>[
+      ...prevELements,
+      {
+        type: 'pencil',
+        offsetX,
+        offsetY,
+        path: [[offsetX, offsetY]],
+        storke: 'black',
+      }
+    ])
     setIsDrawing(true);
   };
   const handleMouseMove = (e) => {
     const { offsetX, offsetY } = e.nativeEvent;
     if (isDrawing) {
-      console.log("offsetX", offsetX);
-      console.log("offsetY", offsetY);
+      const { path } = elements[elements.length - 1];
+      const newPath = [...path, [offsetX, offsetY]];
+      setElements((prevELements)=>
+        prevELements.map((ele, index)=>{
+          if(index === elements.length - 1){
+            return {
+              ...ele,
+              path: newPath,
+            };
+          } else {
+            return ele;
+          }
+        })
+      )
+      // console.log("after", elements);
     }
   };
   const handleMouseUp = (e) => {
     const { offsetX, offsetY } = e.nativeEvent;
-    console.log("offsetX", offsetX);
-    console.log("offsetY", offsetY);
+    // console.log("offsetX", offsetX);
+    // console.log("offsetY", offsetY);
     setIsDrawing(false);
   };
 
